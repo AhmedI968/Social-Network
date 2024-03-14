34e9dccffe9bb70275eb934c6c228f464d2ae14b
Derek Atabayev <derekatabayev4@gmail.com>
2024-03-14 01:39:51 -0600

    removed "test" script from package.json

--- script.ts
+++ script.ts
@@ -3,12 +3,15 @@ const prisma = new PrismaClient() // access to the Prisma client
 
 async function main() { // async = run, wait, run, wait, etc.
     /// ... your Prisma Client queries will go here
-    // const user = await prisma.user.create({
-    //     data: {
-    //         password: 'password', email: 'email', last_active: new Date(), created_at: new Date(), updated_at: new Date(), location: 'location', age: 20
-    //     }
-    // }) // example query to create new user
+    // const user = await prisma.user.createMany({
+    //     data: [
+    //         { name: 'kamran L', password: 'password', email: 'email', last_active: new Date(), created_at: new Date(), updated_at: new Date(), location: 'location', age: 20 },
+    //         { name: 'chef paul', password: 'password', email: 'thelanding', last_active: new Date(), created_at: new Date(), updated_at: new Date(), location: 'location', age: 20 },
+    //     ]
+    // })
+    // example query to create new user
     const findUser = await prisma.user.findMany() // example query to find all users
+    console.log(findUser)
     // const removeUser = await prisma.user.delete({
     //     where: {
     //         user_id: 4
@@ -16,8 +19,8 @@ async function main() { // async = run, wait, run, wait, etc.
     // }) // example query to delete user
     // console.log(user)
     // console.log("Just deleted user ", removeUser)
-    console.log(findUser)
-    await prisma.user.deleteMany() // delete all users
+    // console.log(findUser)
+    // await prisma.user.deleteMany() // delete all users
 }
 
 
