7f4dfb816abcd4059cd48b8f8d5880a13520fef5
Derek Atabayev <derekatabayev4@gmail.com>
2024-04-10 12:13:13 -0600

    dashboard rightbar (at bottom)

--- app/ui/welcome/rightbar/rightbar.tsx
+++ app/ui/welcome/rightbar/rightbar.tsx
@@ -1,5 +1,11 @@
 import styles from './rightbar.module.css';
 import { FcRating } from 'react-icons/fc';
+import { AiOutlineProfile } from 'react-icons/ai';
+import { MdOutlineInterests } from 'react-icons/md';
+import { PrismaClient } from '@prisma/client';
+
+const Prisma = new PrismaClient();
+
 const Rightbar = () => {
     return (
         <div className={styles.container}>
@@ -11,7 +17,24 @@ const Rightbar = () => {
                     <span className={styles.title}>Your Next Match</span>
                     <h3>Curated based on your interests.</h3>
                     <span className={styles.detail}>John Doe</span>
-                    <span className={styles.link}>View Their Profile</span>
+                    <button className={styles.button}>
+                        <AiOutlineProfile />View Their Profile</button>
+                </div>
+            </div>
+
+            <div className={styles.item}>
+                <div className={styles.bgContainer}>
+                    <MdOutlineInterests size={24} />
+                </div>
+                <div className={styles.text}>
+                    <span className={styles.title}>Your Current Interests</span>
+                    <h3>What do you like right now?</h3>
+                    <table className={styles.interests}>
+                        <tr>Music</tr>
+                        <tr>Art</tr>
+                        <tr>Technology</tr>
+                        <tr>Food</tr>
+                    </table>
                 </div>
                 <h2>Rightbar</h2>
                 <p>Rightbar description</p>
