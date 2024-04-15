b4e9756b6de9faa4401f06fa759a61c3e7dda999
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-04-14 22:10:29 -0600

    Created display matches page and incoporated the api

---
+++ app/ui/rating/DisplayMatches/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { use, useState } from 'react';
+import { useRouter } from 'next/router';
+import { fetchAllMatchedUsers } from '@/pages/api/fetchAllMatchedUsers';
+
+const displayMatches = async (user_id : string) => {
+
+    const matchedUsers = await fetchAllMatchedUsers(user_id);
+    const router = useRouter();
+
+    const handleRateButton = (userId: string) => {
+        router.push({
+            pathname: '/app/ui/rating/actualRate/page',
+            query: {user_id: userId}
+        })
+    }
+
+    return(
+        <div>
+            <h1>User Matches</h1>
+            <ul>
+                {matchedUsers.map((user, index) => (
+                    <>
+                        <label>
+                            {user.user_id} - User
+                        </label>
+                        <button onClick={() => handleRateButton(user.user_id)}>
+                            Rate
+                        </button>
+                    </>
+                ))}
+            </ul>
+        </div>
+    )
+};
+
+export default displayMatches;
