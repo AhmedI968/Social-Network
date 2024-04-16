1cfda1d090b550e1765bb557e286a8330e39441d
Derek Atabayev <derekatabayev4@gmail.com>
2024-04-15 22:02:27 -0600

    adding functionality to getScorecard based on user_id: id

--- pages/api/updateUser.ts
+++ pages/api/updateUser.ts
@@ -1,30 +1,42 @@
 "use server";
+import { revalidatePath } from 'next/cache';
 import { prisma } from '../../lib/script';
+import { redirect } from 'next/navigation';
 
 
-export const updateUser = async (formData: any) => {
+export const updateUser = async (formData: FormData) => {
     const { user_id, username, first_name, last_name, email, password, location, age } = Object.fromEntries(formData);
     try {
-        const updateFields = {
-            username: username.toString(),
-            first_name: first_name.toString(),
-            last_name: last_name.toString(),
-            email: email.toString(),
-            password: password.toString(),
-            location: location.toString(),
+        const updateFields: { [key: string]: string | number } = {
+            username: "" + username.toString(),
+            first_name: "" + first_name.toString(),
+            last_name: "" + last_name.toString(),
+            email: "" + email.toString(),
+            password: "" + password.toString(),
+            location: "" + location.toString(),
             age: parseInt(age.toString()) // Convert age to number
         };
 
+        Object.keys(updateFields).forEach(key => {
+            (updateFields[key] === "" || undefined) && delete updateFields[key];
+        });
+
         await prisma.user.update({
-            where: { user_id: user_id.toString() },
+            where: { user_id: user_id.toString() }, // Convert user_id to string
             data: updateFields
         });
-    }
-    catch (err) {
+
+    } catch (err) {
         console.error(err);
         throw new Error('There was an error updating the user');
     }
+
+    revalidatePath('/api/fetchUser');
+    redirect('/profile');
+
 }
+
+
 // export const updateUser = async (formData) => {
 //     const { user_id, username, first_name, last_name, email, password, location, age } = Object.fromEntries(formData);
 //     try {
